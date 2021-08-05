5a6a3cc9ed5f66e09d15c960741cc81a32a45f8f
David <davidsantander9@gmail.com>
2021-08-04 19:47:12 -0500

    change routes in MenuBar

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -16,29 +16,31 @@ export class MenuComponent implements OnInit {
   ngOnInit(): void {
 
     this.items = [
-            {
-                label: 'File',
-                items: [{
-                        label: 'New', 
-                        icon: 'pi pi-fw pi-plus',
-                        items: [
-                            {label: 'Project'},
-                            {label: 'Other'},
-                        ]
-                    },
-                    {label: 'Open'},
-                    {label: 'Quit'}
-                ]
-            },
-            {
-                label: 'Edit',
-                icon: 'pi pi-fw pi-pencil',
-                items: [
-                    {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                    {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-                ]
-            }
-        ];
+      {
+        label: 'Angular pipes',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Text and dates',
+            icon: 'pi pi-align-left',
+            routerLink: '/'
+          },
+          {
+            label: 'Numbers',
+            icon: 'pi pi-dollar',
+            routerLink: 'numbers'
+          },
+          {
+            label: 'No comons',
+            icon: 'pi pi-globe',
+            routerLink: 'no-commons'
+          },
+        ]
+      },{
+        label: 'custom pipes',
+        icon: 'pi pi-cog',
+      }
+  ];
 
   }
 
