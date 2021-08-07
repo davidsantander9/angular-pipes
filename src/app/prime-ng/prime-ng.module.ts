a77daef138bdf3e3241ae81598074dbbf829fac5
David <davidsantander9@gmail.com>
2021-08-06 20:01:30 -0500

    primetable primetoolbar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import {TableModule} from 'primeng/table';
 
 //primeNg
 import { ButtonModule } from 'primeng/button'
 import { CardModule } from 'primeng/card';
-import { MenubarModule } from 'primeng/menubar';
 import { FieldsetModule } from 'primeng/fieldset';
+import { MenubarModule } from 'primeng/menubar';
+import { ToolbarModule } from 'primeng/toolbar';
+
+
 
 @NgModule({
   declarations: [],
@@ -15,6 +19,8 @@ import { FieldsetModule } from 'primeng/fieldset';
     CardModule,
     FieldsetModule,
     MenubarModule,
+    ToolbarModule,
+    TableModule,
   ]
 })
 export class PrimeNgModule { }
