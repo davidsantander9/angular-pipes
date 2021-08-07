6d7c61b13077c94dad320b051f978fa19fac4209
David <davidsantander9@gmail.com>
2021-08-06 20:22:17 -0500

    fill table

--- src/app/sales/pages/sort/sort.component.ts
+++ src/app/sales/pages/sort/sort.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Hero } from '../../interfaces/sales.interfaces';
 
 @Component({
   selector: 'app-sort',
@@ -10,7 +11,33 @@ export class SortComponent{
 
   inUpperCase: boolean = false;
 
-  constructor() { }
+  heroes: Hero[] = [
+    {
+      name: 'Superman',
+      flight: true,
+      color: Color.blue
+    },
+    {
+      name: 'Batman',
+      flight: false,
+      color: Color.black
+    },
+    {
+      name: 'Robin',
+      flight: false,
+      color: Color.green
+    },
+    {
+      name: 'Daredevil',
+      flight: true,
+      color: Color.red
+    },
+    {
+      name: 'Green Lantern',
+      flight: true,
+      color: Color.green
+    },
+  ]
 
   toogle(){
     this.inUpperCase = !this.inUpperCase;
